8025a7a83335fd103a3253917abe19ccb2406e39
Edi Codutti <edi.codutti@gmail.com>
2025-06-10 11:09:28 +0200

    Added tau and Pi functions

--- solver.py
+++ solver.py
@@ -1,5 +1,6 @@
 import gurobipy as gb
 import numpy as np
+import types
 
 class Solver:
     def __init__(self, I, S, J, H, L, A1, A2, A4, B, U, V, C, W, Cp, K, E, P, o, f, G, Gp, O, F, T1, T2, a, d, t, n, m, α, β, γ, θ, λ, τ, M):
@@ -42,6 +43,18 @@ class Solver:
         self.M = M
         self.estimator = gb.Model()
 
+    def tau(self, a, b):
+        if isinstance(self.τ, types.LambdaType) and self.τ.__name__ == "<lambda>":
+            return self.τ(a,b)
+        else:
+            return self.τ
+        
+    def Pi(self, i, j, k):
+        if i in self.T1[j]:
+            k = (~k)&1
+        return self.P[j][k]
+            
+
     def solve(self):
         # Decision Variables
         x = self.estimator.addVars([ (i,s) for i in self.I for s in (self.V[i]+[self.f[i]]) ], vtype=gb.GRB.CONTINUOUS)
@@ -57,7 +70,7 @@ class Solver:
         w2 = self.estimator.addVars([ (j,i,k) for j in self.A4 for k in [1,2] for i in self.T2[j] ], vtype=gb.GRB.BINARY)
 
         # Departure and arrival constraints
-        self.estimator.addConstrs(( y[i,self.o[i]] >= self.d[i,self.o[i]+self.θ]
+        self.estimator.addConstrs(( y[i,self.o[i]] >= self.d[i,self.o[i]] + self.θ
                                    for i in self.I ))
         self.estimator.addConstrs(( y[i,s] >= x[i,s] + self.d[i,s] - self.a[i,s]
                                    for i in self.I for s in self.V[i] ))
@@ -65,11 +78,11 @@ class Solver:
                                    for i in self.I for s in self.C[i] ))
         self.estimator.addConstrs(( y[i,s] >= x[i,s] + self.γ
                                    for i in self.I for s in self.W[i] ))
-        self.estimator.addConstrs(( y[i,s] >= x[i,s] + self.α * z[i,s]
+        self.estimator.addConstrs(( y[i,s] >= x[i,s] + self.α * z[s,i]
                                    for i in self.I for s in set(self.V[i]).intersection(set(self.B)).difference(set(self.C[i])) ))
         self.estimator.addConstrs(( y[i,s] >= self.d[i,s]
                                    for i in self.I for s in self.K[i] ))
-        self.estimator.addConstrs(( x[i,self.P[j][1]] == y[i,self.P[j][0]] + self.t[i,j]
+        self.estimator.addConstrs(( x[i,self.Pi(i,j,1)] == y[i,self.Pi(i,j,0)] + self.t[i,j]
                                    for i in self.I for j in self.E[i] ))
 
         # Deviation calculation constraints
@@ -107,35 +120,35 @@ class Solver:
                                     for s in self.B for i1 in self.G[s] if i1 not in self.Gp[s] ))
         
         # Single track capacity and headway constraints
-        self.estimator.addConstrs(( y[i1,self.P[j][0]] >= x[i2,self.P[j][0]] - self.M * (1-r[self.P[j][1],i2,i1])
+        self.estimator.addConstrs(( y[i1,self.Pi(i1,j,0)] >= x[i2,self.Pi(i2,j,0)] - self.M * (1-r[self.Pi(i1,j,1),i2,i1])
                                    for j in self.A1 for i1 in self.T1[j] for i2 in self.T2[j] ))
-        self.estimator.addConstrs(( y[i2,self.P[j][1]] >= x[i1,self.P[j][1]] - self.M * (1-r[self.P[j][0],i1,i2])
+        self.estimator.addConstrs(( y[i2,self.Pi(i2,j,1)] >= x[i1,self.Pi(i1,j,1)] - self.M * (1-r[self.Pi(i1,j,0),i1,i2])
                                    for j in self.A1 for i1 in self.T1[j] for i2 in self.T2[j] ))
-        self.estimator.addConstrs(( y[i1, self.P[j][0]] >= x[i2, self.P[j][1]] + self.τ - min(self.t[i1,j], self.t[i2,j])
-                                    - self.M * q[self.P[j][0], i1, i2]
+        self.estimator.addConstrs(( y[i1, self.Pi(i1,j,0)] >= x[i2, self.Pi(i2,j,1)] + self.tau(self.t[i1,j],self.t[i2,j]) - min(self.t[i1,j], self.t[i2,j])
+                                    - self.M * q[self.Pi(i1,j,0), i1, i2]
                                    for j in self.A1 for i1 in self.T1[j] for i2 in self.T1[j] if i1 < i2 ))
-        self.estimator.addConstrs(( y[i1, self.P[j][0]] >= x[i2, self.P[j][1]] + self.τ - min(self.t[i1,j], self.t[i2,j])
-                                    - self.M * (1-q[self.P[j][0], i2, i1])
+        self.estimator.addConstrs(( y[i1, self.Pi(i1,j,0)] >= x[i2, self.Pi(i2,j,1)] + self.tau(self.t[i1,j],self.t[i2,j]) - min(self.t[i1,j], self.t[i2,j])
+                                    - self.M * (1-q[self.Pi(i1,j,0), i2, i1])
                                    for j in self.A1 for i1 in self.T1[j] for i2 in self.T1[j] if i2 < i1 ))
-        self.estimator.addConstrs(( y[i1, self.P[j][1]] >= x[i2, self.P[j][0]] + self.τ - min(self.t[i1,j], self.t[i2,j])
-                                    - self.M * q[self.P[j][1], i1, i2]
+        self.estimator.addConstrs(( y[i1, self.Pi(i1,j,1)] >= x[i2, self.Pi(i2,j,0)] + self.tau(self.t[i1,j],self.t[i2,j]) - min(self.t[i1,j], self.t[i2,j])
+                                    - self.M * q[self.Pi(i1,j,1), i1, i2]
                                    for j in self.A1 for i1 in self.T2[j] for i2 in self.T2[j] if i1 < i2 ))
-        self.estimator.addConstrs(( y[i1, self.P[j][1]] >= x[i2, self.P[j][0]] + self.τ - min(self.t[i1,j], self.t[i2,j])
-                                    - self.M * (1-q[self.P[j][1], i2, i1])
+        self.estimator.addConstrs(( y[i1, self.Pi(i1,j,1)] >= x[i2, self.Pi(i2,j,0)] + self.tau(self.t[i1,j],self.t[i2,j]) - min(self.t[i1,j], self.t[i2,j])
+                                    - self.M * (1-q[self.Pi(i1,j,1), i2, i1])
                                    for j in self.A1 for i1 in self.T2[j] for i2 in self.T2[j] if i2 < i1 ))
         
         # Double track and headway constraints
-        self.estimator.addConstrs(( y[i1, self.P[j][0]] >= x[i2, self.P[j][1]] + self.τ - min(self.t[i1,j], self.t[i2,j])
-                                    - self.M * q[self.P[j][0], i1, i2]
+        self.estimator.addConstrs(( y[i1, self.Pi(i1,j,0)] >= x[i2, self.Pi(i2,j,1)] + self.tau(self.t[i1,j],self.t[i2,j]) - min(self.t[i1,j], self.t[i2,j])
+                                    - self.M * q[self.Pi(i1,j,0), i1, i2]
                                    for j in self.A2 for i1 in self.T1[j] for i2 in self.T1[j] if i1 < i2 ))
-        self.estimator.addConstrs(( y[i1, self.P[j][0]] >= x[i2, self.P[j][1]] + self.τ - min(self.t[i1,j], self.t[i2,j])
-                                    - self.M * (1-q[self.P[j][0], i2, i1])
+        self.estimator.addConstrs(( y[i1, self.Pi(i1,j,0)] >= x[i2, self.Pi(i2,j,1)] + self.tau(self.t[i1,j],self.t[i2,j]) - min(self.t[i1,j], self.t[i2,j])
+                                    - self.M * (1-q[self.Pi(i1,j,0), i2, i1])
                                    for j in self.A2 for i1 in self.T1[j] for i2 in self.T1[j] if i2 < i1 ))
-        self.estimator.addConstrs(( y[i1, self.P[j][1]] >= x[i2, self.P[j][0]] + self.τ - min(self.t[i1,j], self.t[i2,j])
-                                    - self.M * q[self.P[j][1], i1, i2]
+        self.estimator.addConstrs(( y[i1, self.Pi(i1,j,1)] >= x[i2, self.Pi(i2,j,0)] + self.tau(self.t[i1,j],self.t[i2,j]) - min(self.t[i1,j], self.t[i2,j])
+                                    - self.M * q[self.Pi(i1,j,1), i1, i2]
                                    for j in self.A2 for i1 in self.T2[j] for i2 in self.T2[j] if i1 < i2 ))
-        self.estimator.addConstrs(( y[i1, self.P[j][1]] >= x[i2, self.P[j][0]] + self.τ - min(self.t[i1,j], self.t[i2,j])
-                                    - self.M * (1-q[self.P[j][1], i2, i1])
+        self.estimator.addConstrs(( y[i1, self.Pi(i1,j,1)] >= x[i2, self.Pi(i2,j,0)] + self.tau(self.t[i1,j],self.t[i2,j]) - min(self.t[i1,j], self.t[i2,j])
+                                    - self.M * (1-q[self.Pi(i1,j,1), i2, i1])
                                    for j in self.A2 for i1 in self.T2[j] for i2 in self.T2[j] if i2 < i1 ))
         
         # Quadruple track and headway constraints
@@ -143,17 +156,17 @@ class Solver:
                                    for j in self.A4 for i in self.T1[j] ))
         self.estimator.addConstrs(( gb.quicksum(w2[j,i,k] for k in [1,2]) == 1
                                    for j in self.A4 for i in self.T2[j] ))
-        self.estimator.addConstrs(( y[i1,self.P[j][0]] >= x[i2, self.P[j][1]] + self.τ - min(self.t[i1,j], self.t[i2,j])
-                                    - self.M * (2 + q[self.P[j][0], i1, i2] - w1[j,i1,k] - w1[j,i2,k])
+        self.estimator.addConstrs(( y[i1,self.Pi(i1,j,0)] >= x[i2, self.Pi(i2,j,1)] + self.tau(self.t[i1,j],self.t[i2,j]) - min(self.t[i1,j], self.t[i2,j])
+                                    - self.M * (2 + q[self.Pi(i1,j,0), i1, i2] - w1[j,i1,k] - w1[j,i2,k])
                                     for j in self.A4 for k in [1,2] for i1 in self.T1[j] for i2 in self.T1[j] if i1 < i2 ))
-        self.estimator.addConstrs(( y[i1,self.P[j][0]] >= x[i2, self.P[j][1]] + self.τ - min(self.t[i1,j], self.t[i2,j])
-                                    - self.M * (3 - q[self.P[j][0], i2, i1] - w1[j,i1,k] - w1[j,i2,k])
+        self.estimator.addConstrs(( y[i1,self.Pi(i1,j,0)] >= x[i2, self.Pi(i2,j,1)] + self.tau(self.t[i1,j],self.t[i2,j]) - min(self.t[i1,j], self.t[i2,j])
+                                    - self.M * (3 - q[self.Pi(i1,j,0), i2, i1] - w1[j,i1,k] - w1[j,i2,k])
                                     for j in self.A4 for k in [1,2] for i1 in self.T1[j] for i2 in self.T1[j] if i2 < i1 ))
-        self.estimator.addConstrs(( y[i1,self.P[j][2]] >= x[i2, self.P[j][0]] + self.τ - min(self.t[i1,j], self.t[i2,j])
-                                    - self.M * (2 + q[self.P[j][1], i1, i2] - w2[j,i1,k] - w2[j,i2,k])
+        self.estimator.addConstrs(( y[i1,self.P[j][2]] >= x[i2, self.Pi(i2,j,0)] + self.tau(self.t[i1,j],self.t[i2,j]) - min(self.t[i1,j], self.t[i2,j])
+                                    - self.M * (2 + q[self.Pi(i1,j,1), i1, i2] - w2[j,i1,k] - w2[j,i2,k])
                                     for j in self.A4 for k in [1,2] for i1 in self.T2[j] for i2 in self.T2[j] if i1 < i2 ))
-        self.estimator.addConstrs(( y[i1,self.P[j][1]] >= x[i2, self.P[j][0]] + self.τ - min(self.t[i1,j], self.t[i2,j])
-                                    - self.M * (3 - q[self.P[j][1], i2, i1] - w2[j,i1,k] - w2[j,i2,k])
+        self.estimator.addConstrs(( y[i1,self.Pi(i1,j,1)] >= x[i2, self.Pi(i2,j,0)] + self.tau(self.t[i1,j],self.t[i2,j]) - min(self.t[i1,j], self.t[i2,j])
+                                    - self.M * (3 - q[self.Pi(i1,j,1), i2, i1] - w2[j,i1,k] - w2[j,i2,k])
                                     for j in self.A4 for k in [1,2] for i1 in self.T2[j] for i2 in self.T2[j] if i2 < i1 ))
         
         # Drop off/Pick up capacity constraint for non-yard stations
